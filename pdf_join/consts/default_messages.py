9c655c7320d2b6490b823cf702423c3d47198c9b
lipe <luisfelipe.refosco@hotmail.com>
2023-03-15 18:03:24 -0300

    feat: add help messagem

--- pdf_join/consts/default_messages.py
+++ pdf_join/consts/default_messages.py
@@ -1,5 +1,8 @@
+help_message = "[COMMAND] [--OPTION] file1 file2 ... fileN"
+
 DEFAULT_MESSAGES = {
-    "NO_INPUT_FILES": "Insert files to join PDF's",
-    "INVALID_OPTION": "Invalid option!",
-    "NO_COMMAND": "Show help message..."
+    "NO_INPUT_FILES": "Insert files to join PDF's \n" + help_message,
+    "INVALID_OPTION": "Invalid option! \n" + help_message,
+    "HELP": help_message,
+    "NO_COMMAND": "No command! \n" + help_message
 }
