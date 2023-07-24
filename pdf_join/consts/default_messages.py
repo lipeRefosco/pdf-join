1d2315dd34a1ad90b011a1491fcf48563054d94b
lipe <luisfelipe.refosco@hotmail.com>
2023-07-24 13:33:50 -0300

    style: emprove help message

--- pdf_join/consts/default_messages.py
+++ pdf_join/consts/default_messages.py
@@ -1,4 +1,11 @@
-help_message = "[COMMAND] [--OPTION] file1 file2 ... fileN"
+from .commands import VALID_COMMANDS
+from .options import INPUT_OPTION, OUTPUT_OPTION
+
+help_message =  "[COMMAND] [--OPTION] file1 file2 ... fileN\n\n" + \
+                "Commands: {}\n".format(VALID_COMMANDS) + \
+                "Options:\n" + \
+                "   {}: Insert file path for all files.\n".format(', '.join(INPUT_OPTION)) + \
+                "   {}: Path for the output. Default is the same path of program.".format(', '.join(OUTPUT_OPTION))
 
 DEFAULT_MESSAGES = {
     "NO_INPUT_FILES": "Insert files to join PDF's \n" + help_message,
